b98063cafe3bef57c683a5242bdfdd3c3636a5c1
Saif Ahmed <saifad303@gmail.com>
2022-03-14 14:58:09 +0600

    branch main fresh

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'protectedAge' => \App\Http\Middleware\checkAgeRoute::class,
     ];
 }
